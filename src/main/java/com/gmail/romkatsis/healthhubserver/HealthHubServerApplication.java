dbabcf9f1fa5a567cf6a2b0ff47efa34932dd8de
RomanCamelCase <romkatsis@gmail.com>
2024-04-21 18:00:05 +0200

    Init commit

---
+++ src/main/java/com/gmail/romkatsis/healthhubserver/HealthHubServerApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.romkatsis.healthhubserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHubServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthHubServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/romkatsis/healthhubserver/HealthHubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.romkatsis.healthhubserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthHubServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
