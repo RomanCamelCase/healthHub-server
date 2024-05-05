a43751ea2e51ea0f4fe37e80f4f90e911439dcb5
RomanCamelCase <romkatsis@gmail.com>
2024-05-06 00:57:41 +0200

    Added exception handlers: handleValidationError, handleMessageNotReadableError, handleUnexpectedError

--- src/main/java/com/gmail/romkatsis/healthhubserver/controllers/ExceptionController.java
+++ src/main/java/com/gmail/romkatsis/healthhubserver/controllers/ExceptionController.java
@@ -1,23 +1,67 @@
 package com.gmail.romkatsis.healthhubserver.controllers;
 
 import com.gmail.romkatsis.healthhubserver.dtos.PlainErrorResponse;
+import com.gmail.romkatsis.healthhubserver.dtos.ValidationErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 @RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(AuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public PlainErrorResponse handleAuthenticationError(AuthenticationException exception, HttpServletRequest request) {
+    public PlainErrorResponse handleAuthenticationError(AuthenticationException exception,
+                                                        HttpServletRequest request) {
         return new PlainErrorResponse(
                 HttpStatus.UNAUTHORIZED.value(),
                 request.getServletPath(),
                 exception.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleValidationError(MethodArgumentNotValidException exception,
+                                                         HttpServletRequest request) {
+
+        Map<String, List<String>> errors = exception.getFieldErrors()
+                .stream().collect(Collectors.groupingBy(
+                        FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())));
+
+        return new ValidationErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                request.getServletPath(),
+                "Form validation error. For details, review the validationErrors field",
+                errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public PlainErrorResponse handleMessageNotReadableError(HttpServletRequest request) {
+        return new PlainErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                request.getServletPath(),
+                "Cannot read request content");
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public PlainErrorResponse handleUnexpectedError(HttpServletRequest request) {
+        return new PlainErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                request.getServletPath(),
+                "An internal server error occurred");
+    }
 }
---
+++ src/main/java/com/gmail/romkatsis/healthhubserver/dtos/ValidationErrorResponse.java
@@ -0,0 +1,25 @@
+package com.gmail.romkatsis.healthhubserver.dtos;
+
+import java.util.List;
+import java.util.Map;
+
+public class ValidationErrorResponse extends PlainErrorResponse {
+
+    private Map<String, List<String>> validationErrors;
+
+    public ValidationErrorResponse(int statusCode,
+                                   String path,
+                                   String message, Map<String, List<String>> validationErrors) {
+        super(statusCode, path, message);
+        this.validationErrors = validationErrors;
+    }
+
+    public Map<String, List<String>> getValidationErrors() {
+        return validationErrors;
+    }
+
+    public void setValidationErrors(Map<String, List<String>> validationErrors) {
+        this.validationErrors = validationErrors;
+    }
+}
+
